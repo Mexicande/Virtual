e58014b469bf9058086cf8eca9323b941d6f327b
mexicande <mexicande@hotmail.com>
2018-03-15 19:26:47 +0800

    add utils

--- app/src/main/java/cn/com/virtualbitcoin/MainActivity.java
+++ app/src/main/java/cn/com/virtualbitcoin/MainActivity.java
@@ -3,9 +3,6 @@ package cn.com.virtualbitcoin;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-/**
- * @author apple
- */
 public class MainActivity extends AppCompatActivity {
 
     @Override
